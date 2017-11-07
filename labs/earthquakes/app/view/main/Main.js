c3755fe17d16bf1958db3837a596c9a63e87ef71
fdalbo <frederic.dalbo@agilum.com>
2017-11-07 15:49:15 +0100

    update

--- labs/earthquakes/app/view/main/Main.js
+++ labs/earthquakes/app/view/main/Main.js
@@ -1,32 +1,43 @@
 Ext.define('Earthquakes.view.main.Main', {
-    extend: 'Ext.panel.Panel',
-    xtype: 'main',
-    requires: [
-        'Earthquakes.view.Grid',
-        'Ext.plugin.Viewport'
-    ],
-    layout: 'border',
-    title: 'Earthquakes in Iceland',
-    items: [{
-        xtype: 'earthquakesgrid',
-        region: 'center',
-        store: {
-            type: 'store',
-            model: 'Ext.data.Model',
-            fields: [{
-                type: 'date',
-                name: 'timestamp',
-                dateFormat: 'c'
-            }],
-            sorters: ['timestamp'],
-            autoLoad: true,
-            proxy: {
-                type: 'ajax',
-                url: '//apis.is/earthquake/is',
-                reader: {
-                    rootProperty: 'results'
-                }
-            }
-        }
-    }]
+	extend : 'Ext.panel.Panel',
+	xtype : 'main',
+	requires : [ 	'Earthquakes.view.Grid',
+					'Ext.plugin.Viewport',
+					'Earthquakes.view.Map',
+			        'Earthquakes.view.main.MainModel',
+			        'Ext.window.Toast',
+			        'Earthquakes.view.main.MainController'
+				],
+	controller: 'main',
+	layout : 'border',
+	title : 'Earthquakes in Iceland',
+	
+    viewModel: {
+        type: 'main'
+    },
+	
+	items : [ {
+		xtype : 'earthquakesmap',
+		region : 'north',
+		flex : 1,
+		location : {
+			latitude : 64.9312762,
+			longitude : -19.021169
+		},
+		split : true,    
+		listeners: {
+	        select: 'onSelect'
+	    },
+	    bind: {
+	        store: '{earthquakes}',
+	        selection: '{earthquake}'
+	    }
+	}, {
+		xtype : 'earthquakesgrid',
+		region : 'center',
+	    bind: {
+	        store: '{earthquakes}',
+	        selection: '{earthquake}'
+	    }
+	} ]
 });
